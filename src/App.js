9fa6d9e1e7df09b8d4db49b72606e2822d6d1a11
alexzarembovskiy <alexzarembovskiy@gmail.com>
2022-10-26 18:16:53 +0200

    added expose 80

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Pupa Lupa
+          I love you :*
         </p>
         <a
           className="App-link"
